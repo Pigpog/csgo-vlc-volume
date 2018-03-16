363543894dcc37cd38697d833810227ec542a7ba
Pigpog <7060603@hotmail.ca>
2018-03-16 17:13:41 -0400

    Efficiency Upgrade

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ http = require('http');
 fs = require('fs');
 var vlcService = require("droopy-vlc"),
     vlc = new vlcService("http://:"+config.VLCpassword+"@localhost:"+config.VLCport);
-
 var currvol=0;
+let lightsOn=false;
+
+function toggleVolume(up){
+	if(up && currvol!==config.upvol){
+		vlc.volume(config.upvol)
+		currvol=config.upvol
+	}else if(!up && currvol!==config.downvol){
+		vlc.volume(config.downvol)
+		currvol=config.downvol
+	}
+}
 
 //https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Game_State_Integration#Sample_HTTP_POST_Endpoint_Server
-
 server = http.createServer( function(req, res) {
     if (req.method == 'POST') {
         console.log("Handling POST request...");
@@ -20,19 +29,20 @@ server = http.createServer( function(req, res) {
         req.on('end', function () {
 			bodyParsed=JSON.parse(body);
 			if(bodyParsed.player){
-				if(bodyParsed.player.state.health===0){	//if health is 0 (dead)
-					if(currvol!==config.upvol){
-						vlc.volume(config.upvol)
+					console.log("youre playing");
+					if(bodyParsed.player.state.health===0){	//if health is 0 (dead)
+						if(currvol!==config.upvol){
+							toggleVolume(true);
+						}
+					}else{
+						if(currvol!==config.downvol){
+							toggleVolume(false);
+						}
 					}
-				}else{
-					if(currvol!==config.downvol){
-						vlc.volume(config.downvol)
-					}
-				}
 			}
 			if(bodyParsed.round){
 				if(bodyParsed.round.phase==="over"){
-					vlc.volume(config.upvol)
+					toggleVolume(true);
 				}
 			}
         	res.end( '' );
